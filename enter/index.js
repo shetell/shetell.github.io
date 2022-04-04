4cc33ff89daad52acac15be1e97551a372cdaed5
shetell <71158490+shetell@users.noreply.github.com>
2022-04-04 15:35:43 +0800

    Delete enter directory

--- enter/index.js
+++
@@ -1,8 +0,0 @@
-mytime()
-function mytime(){
-    var time = document.getElementById('txt')
-    var a = new Date(); 
-    var b = a.getHours()+':'+a.getMinutes()+':'+a.getSeconds();
-    var c = a.getFullYear()+'-'+a.getMonth()+'-'+a.getDay();
-    time.firstChild.nodeValue=b+" "+c+"  "+b+" "+c+" "+b+" "+c+" "+b+" "+c+" "+b+" "+c+" "+b+" "+c+" "+b+" "+c+" "+b+" "+c+" "+b+" "+c+" "+b+" "+c; 
-}
